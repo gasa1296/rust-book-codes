09d9c54705dec64fa9147268e22ed5a47f3140e9
Gabriel Alejandro <gabriel.asa.1296@outlook.es>
2020-11-23 23:19:22 -0400

    Finished

--- 10. Generic Types, Traits, and Lifetimes/validating_references_with_lifetimes/src/main.rs
+++ 10. Generic Types, Traits, and Lifetimes/validating_references_with_lifetimes/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 //Lifetime Annotation Syntax
 //&i32 a reference
 //&'a i32 a reference with an explicit lifetime
@@ -8,8 +9,29 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
-}
 
+    //Lifetime Annotations in Struct Definitions
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    //The Static Lifetime
+    let s: &'static str = "I have a static lifetime.";
+}
+//Lifetime Annotations in Method Definitions
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
 //Lifetime Annotations in Function Signatures
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
@@ -18,3 +40,19 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         y
     }
 }
+//Generic Type Parameters, Trait Bounds, and Lifetimes Together
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display 
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+//Lifetime Elision
+//Lifetime rules:
+//The first rule is that each parameter that is a reference gets its own lifetime parameter
+//The second rule is if there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters
+//The third rule is if there are multiple input lifetime parameters, but one of them is &self or &mut self because this is a method, the lifetime of self is assigned to all output lifetime parameters
