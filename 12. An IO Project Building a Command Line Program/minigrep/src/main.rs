1da9bbcca9342e3ca3a27487b3556ad4c09eb8dc
Gabriel Segovia <gabriel.asa.1296@outlook.es>
2021-02-03 19:35:25 -0400

    Improving Our I/O Project
    using iterators

--- 12. An IO Project Building a Command Line Program/minigrep/src/main.rs
+++ 12. An IO Project Building a Command Line Program/minigrep/src/main.rs
@@ -1,13 +1,12 @@
-uue std::env;
+use std::env;
 use std::process;
 
 use minigrep::Config;
 
 fn main() 
 {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::new( &args ).unwrap_or_else( |err| {
+    let config = Config::new( env::args() ).unwrap_or_else( |err| {
         eprintln!( "Problem parsing arguments: {}", err );
         process::exit( 1 );
     });
@@ -21,5 +20,6 @@ fn main()
 
         process::exit( 1 );
     }
+
 }
 
